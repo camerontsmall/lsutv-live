c1dafceead9a53e5d6cdabe0c5e10e85b7722123
Cameron Small <camerontsmall@googlemail.com>
2017-10-19 19:07:02 +0100

    actually let's not use webpack

--- web/src/components/home.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export interface HomeProps { compiler : string; framework : string; }
-
-export class Home extends React.Component<HomeProps, {}> {
-    render() {
-        return <h1>Home Component</h1>;
-    }
-}
--- web/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { Home } from "./components/home";
-
-ReactDOM.render(
-    <Home compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
