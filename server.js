a5d25dfa29ca700198cb21405e485e2e0814c9a4
Cameron Small <camerontsmall@googlemail.com>
2017-10-19 23:40:46 +0100

    more work on frontend. made basic page structure.

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ var app = express();
 
 var server = http.createServer(app);
 
-app.use(express.static('web'));
+/* Enable CORS so we can use CDN resources */
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.use(express.static('public'));
 
 if(Number.isInteger(config.port) == true){
     
